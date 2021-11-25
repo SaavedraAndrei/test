5d332baf923e9791fd20ebdd0f78b186d6d5784d
Andrei Saavedra <andreisr0105@gmail.com>
2021-11-25 10:57:30 -0500

    test-2

--- resources/views/certificado.blade.php
+++ resources/views/certificado.blade.php
@@ -13,19 +13,19 @@
 <body>
 
 
-    <div class=" certificado-container" style="border: 20px dotted #887564; background-color: #F7EBD5; padding: 50px;">
+    <div class=" certificado-container" style="border: 20px dotted #123360; padding: 50px;">
 
 
-        <h1 class="certificado-title" style="text-align: center; color: black; font-size: 50px; ">Certificado de Participación</h1>
+        <h1 class="certificado-title" style="text-align: center; color: #DAC549; font-size: 50px; ">Certificado de Participación</h1>
 
         <p style="text-align: center; margin-top: 45px; font-size: 15px">Por medio del presente reconocemos a:</p>
 
-        <H2 style="text-align: center; margin-top: 0px; font-size: 35px; font-style: italic;">{{$nombre_usuario}}</H2>
+        <H2 style="text-align: center; margin-top: 0px; font-size: 35px; font-style: italic; color: #DAC549;">{{$nombre_usuario}}</H2>
 
         <P style="font-size: 16px;text-align: center; width: 60px;">por haber participado en el congreso de desarrollo y diseño de aplicaciones web</P>
 
         <p style="text-align: center;margin-top: 70px">______________________________</p>
-        <p style="text-align: center;font-size: 14px; text-transform: uppercase; font-weight: bold ">Andrei Saavedra Rivera</p>
+        <p style="text-align: center;font-size: 14px; text-transform: uppercase; font-weight: bold; color: #DAC549;">Cesar Lino Rosado Salas</p>
         <p style="text-align: center;font-size: 13px;color: #8c8c8c; ">Gerente General</p>
 
     </div>
